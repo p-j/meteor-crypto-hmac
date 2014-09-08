84529a5d3acce791ff70a6d4fb0bc1e8a78e4d55
Jeremie Parker <jparker@guimiesoft.com>
2014-09-08 17:35:42 +0200

    Update for Meteor 0.9.x new package system

--- package.js
+++ package.js
@@ -1,15 +1,18 @@
 Package.describe({
-  summary: "HMAC function for CryptoJS, standard secure crypto algorithms"
+	summary: 'HMAC function for CryptoJS, standard secure crypto algorithms',
+	version: '3.1.2-1',
+	git: 'https://github.com/p-j/meteor-crypto-hmac'
 });
 
 Package.on_use(function (api) {
-  api.use('crypto-base',    ['client', 'server']);
-  api.imply('crypto-base',  ['client', 'server']);
-  api.add_files(['hmac.js'], ['client', 'server']);
+	api.versionsFrom('METEOR@0.9.0');
+	api.use('jparker:meteor-crypto-base@3.1.2-1', ['client', 'server']);
+	api.imply('jparker:meteor-crypto-base', ['client', 'server']);
+	api.add_files(['hmac.js'], ['client', 'server']);
 });
 
 Package.on_test(function (api) {
-  api.use(['crypto-base', 'crypto-hmac', 'crypto-sha1', 'crypto-md5', 'tinytest']);
+	api.use(['jparker:meteor-crypto-base', 'jparker:meteor-crypto-hmac', 'jparker:meteor-crypto-sha1', 'jparker:meteor-crypto-md5', 'tinytest']);
 
-  api.add_files('crypto-hmac_tests.js', ['client', 'server']);
+	api.add_files('crypto-hmac_tests.js', ['client', 'server']);
 });
